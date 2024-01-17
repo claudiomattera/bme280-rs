d3a63fa6bb7ce6ab623fac5ae581421a5e3199be
Claudio Giovanni Mattera <dev@claudiomattera.it>
2024-01-17 07:17:46 +0100

    Move lints configuration to Cargo.toml

--- src/lib.rs
+++ src/lib.rs
@@ -8,44 +8,6 @@
 
 #![cfg_attr(not(doctest), doc = include_str!("../Readme.md"))]
 #![cfg_attr(not(test), no_std)]
-#![deny(
-    missing_docs,
-    clippy::cargo,
-    clippy::pedantic,
-    clippy::missing_docs_in_private_items,
-    clippy::missing_errors_doc,
-    clippy::missing_panics_doc
-)]
-#![deny(
-    clippy::alloc_instead_of_core,
-    clippy::allow_attributes_without_reason,
-    clippy::clone_on_ref_ptr,
-    clippy::else_if_without_else,
-    clippy::expect_used,
-    clippy::format_push_string,
-    clippy::if_then_some_else_none,
-    clippy::let_underscore_must_use,
-    clippy::lossy_float_literal,
-    clippy::panic,
-    clippy::panic_in_result_fn,
-    clippy::pattern_type_mismatch,
-    clippy::rc_buffer,
-    clippy::rc_mutex,
-    clippy::rest_pat_in_fully_bound_structs,
-    clippy::self_named_module_files,
-    clippy::std_instead_of_alloc,
-    clippy::std_instead_of_core,
-    clippy::str_to_string,
-    clippy::string_slice,
-    clippy::string_to_string,
-    clippy::todo,
-    clippy::undocumented_unsafe_blocks,
-    clippy::unimplemented,
-    clippy::unreachable,
-    clippy::unseparated_literal_suffix,
-    clippy::unwrap_used,
-    clippy::verbose_file_reads
-)]
 
 mod bme280;
 pub use crate::bme280::{Bme280, Sample, CHIP_ID, DEFAULT_ADDRESS};
